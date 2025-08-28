5af592911f5ed82022c91cf5625316f7fce478c7
Aakash Pandey <aakash@aiyensi.com>
2025-08-28 18:25:21 +0530

    temprory chnages

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -5,10 +5,9 @@ const Booking = () => {
     return (
         <div>
             <div className="flex justify-between items-center p-4 bg-gray-100">
-                <h1>Booking</h1>
+                <h1>Booking / first booking</h1>
                 <UserMenu />
             </div>
-            {/* Other booking page content */}
         </div>
     );
 };
